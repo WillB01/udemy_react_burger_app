b012d3b20134483e17ee7ba3b79831cea883bb6b
WillB01 <wb.william@outlook.com>
2018-10-03 10:57:22 +0200

    adding stuff

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,32 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/Burgeringredient';
 
 const burger = (props) => {
+    console.log(props.ingredients);
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+            console.log(igKey);
+            return [...Array(props.ingredients[igKey])]
+                .map((_, index) => {
+                    console.log(transformedIngredients);
+                    return <BurgerIngredient key={igKey + index}
+                                      type={igKey} />
+                });
+        })
+        .reduce((arr, el) => {
+            console.log(`[Inside Reduce] arr: ${arr} --- el: ${el}`)
+            return arr.concat(el);
+        }, []);
+        console.log(transformedIngredients);
+    if (transformedIngredients.length === 0) {
+       transformedIngredients = <p>Please start adding ingredients</p> 
+    }
+
+    
     return(
         <div className={classes.Burger} >
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
+            <BurgerIngredient type ="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type ="bread-bottom" />
         </div>
     );
 };
