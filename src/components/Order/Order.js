366cdc0dc475038fe586a36423ba8ea563d4cb3e
WillB01 <wb.william@outlook.com>
2018-10-10 14:17:04 +0200

    order done

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classes from './Order.css';
 
-
 const order = (props) => {
     const ingredients = [];
 
@@ -11,15 +10,25 @@ const order = (props) => {
             amount: props.ingredients[ingredientName]
         });
     };
+
     
+
     const ingredientOutput = ingredients.map((ig, index) => {
         return(
-            <span key={index}>{ig.name} ({ig.amount}) </span>
+            <span style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+
+                }} 
+                  key={ig.name}>{ig.name} ({ig.amount})</span>
         )
     });
     return(
         <div className={classes.Order}>
-            {ingredientOutput}
+            Ingredients: {ingredientOutput}
             <p>Price: <strong>USD {props.price}</strong></p>
         </div>
     );
