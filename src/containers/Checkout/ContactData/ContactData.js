fc829aa0368b6ab26f8945ea2689fc8f11c86b21
WillB01 <william.hello@outlook.com>
2018-10-11 12:53:43 +0200

    adding validation css

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -20,7 +20,8 @@ class ContactData extends Component {
                     validation: {
                         required: true
                     },
-                    valid: false
+                    valid: false,
+                    touched: false
                 },
                 street: {
                     elementType: 'input',
@@ -32,7 +33,8 @@ class ContactData extends Component {
                     validation: {
                         required: true
                     },
-                    valid: false
+                    valid: false,
+                    touched: false
                 },
                 zipCode: {
                     elementType: 'input',
@@ -42,9 +44,12 @@ class ContactData extends Component {
                     },
                     value: '',
                     validation: {
-                        required: true
+                        required: true,
+                        minLength: 5,
+                        maxLength: 5
                     },
-                    valid: false
+                    valid: false,
+                    touched: false
                 },
                 country: {
                     elementType: 'input',
@@ -56,7 +61,8 @@ class ContactData extends Component {
                     validation: {
                         required: true
                     },
-                    valid: false
+                    valid: false,
+                    touched: false
                 },
                 email: {
                     elementType: 'input',
@@ -68,7 +74,8 @@ class ContactData extends Component {
                     validation: {
                         required: true
                     },
-                    valid: false
+                    valid: false,
+                    touched: false
                 },
                 deleveryMethod: {
                     elementType: 'select',
@@ -84,7 +91,7 @@ class ContactData extends Component {
                         },
                     ]
                     },
-                    value: '',
+                    value: ''
                     
                 },
             },
@@ -123,14 +130,18 @@ class ContactData extends Component {
     };
 
     checkValidity(value, rules) {
-        let isValid = false;
+        let isValid = true;
         
         if (rules.required) {
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
         } 
 
-        if(rules.minLength) {
-            isValid = value.length >= rules.minLength;
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
         }
 
         return isValid;
@@ -146,6 +157,7 @@ class ContactData extends Component {
         updatedFormElement.valid = 
             this.checkValidity(updatedFormElement.value,
                                 updatedFormElement.validation);
+        updatedFormElement.touched = true;          
         console.log(updatedFormElement);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         this.setState({orderForm: updatedOrderForm});
@@ -171,6 +183,9 @@ class ContactData extends Component {
                            elementType={formElement.config.elementType} 
                            elementConfig={formElement.config.elementConfig}
                            value={formElement.config.value}
+                           invalid={!formElement.config.valid}
+                           shouldValidate={formElement.config.validation}
+                           touched={formElement.config.touched}
                            changed={(event) =>
                                 this.inputChangedHandler(event, formElement.id)}/>
                 ))}
