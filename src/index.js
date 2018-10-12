3b7fdceae52fa1b3bbb36a0e01de332a80a974b2
WillB01 <william.hello@outlook.com>
2018-10-12 14:01:33 +0200

    adding redux to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+import reducer from './store/reducers/reducer';
+
+import {BrowserRouter} from 'react-router-dom';
+import {combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const rootReducer = combineReducers({
+    brg: reducer,
+});
+
+const store = createStore(rootReducer);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
