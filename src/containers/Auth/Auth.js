7da8c3b950e96670c7499b548336958ccd9d2ab1
WillB01 <william.hello@outlook.com>
2018-10-17 15:37:45 +0200

    fixing ui

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,8 +5,7 @@ import classes from './Auth.css';
 import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 
 class Auth extends Component {
@@ -116,9 +115,15 @@ class Auth extends Component {
                 <p>{this.props.error.message}</p>
             );
         }
+
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to="/" /> ;
+        };
         
         return(
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                    {form} 
@@ -136,7 +141,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../../../store/actions/index';
+import { Redirect} from 'react-router-dom';
 
 export class Logout extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+    componentDidMount() {
+        this.props.onLogout();
+    };
 
-export default Logout
+  render() {
+    return <Redirect to="/" /> ;
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(actions.logout())
+    };
+};
+
+
+export default connect(null, mapDispatchToProps)(Logout);
+
