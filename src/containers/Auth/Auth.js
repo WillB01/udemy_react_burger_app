9f2c8464abd19efd41a740c0bcd11829cea4ac2f
WillB01 <william.hello@outlook.com>
2018-10-18 12:20:52 +0200

    adding updateObject in more files

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,7 +6,7 @@ import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import {Redirect} from 'react-router-dom';
-
+import {updateObject} from '../../shared/utility';
 
 class Auth extends Component {
     state = {
@@ -68,15 +68,14 @@ class Auth extends Component {
     }
 
     inputChangedHandler = (event, controlName) => {
-            const updatedOrderForm = { 
-                ...this.state.controls,
-                [controlName]: {
-                    ...this.state.controls[controlName],
+            const updatedOrderForm = updateObject(this.state.controls, {
+                [controlName]: updateObject(...this.state.controls[controlName],{
                     value: event.target.value,
                     valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                     touched: true
-                }
-            };
+                })
+            });
+
             this.setState({controls: updatedOrderForm});
     };
 
