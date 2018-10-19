8841f575b66bdfbf1faf4c8f3ff09d1166f4efe0
WillB01 <william.hello@outlook.com>
2018-10-19 13:08:47 +0200

    adding sagas

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
 import burgerBuilder from './store/reducers/burgerBuilder';
 import order from './store/reducers/order';
-
 import {BrowserRouter} from 'react-router-dom';
 import {combineReducers, createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import authReducer from './store/reducers/auth';
+import createSagaMiddleware from 'redux-saga';
+import {watchAuth} from './store/sagas/index'; 
 
 const composeEnhancers = process.env.NODE_ENV === 'development' 
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null|| compose;
@@ -23,10 +22,13 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer,composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
 
+sagaMiddleware.run(watchAuth);
 const app = (
     <Provider store={store}>
         <BrowserRouter>
